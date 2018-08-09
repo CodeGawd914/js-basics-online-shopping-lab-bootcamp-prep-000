a65df6bf740978e7e5fea6071321032508f8f91c
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-09 04:07:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function placeOrder(cardNumber) {
     `Your total cost is $${total} which will be charged to the card ${cardNumber}.`;
     for (var i = cart.length; i>=0;i--){
       cart.pop();
-    } else { "Sorry, we don't have a credit card on file for you"
+    } else { ("Sorry, we don\'t have a credit card on file for you")
     return cart
     }}
   }
