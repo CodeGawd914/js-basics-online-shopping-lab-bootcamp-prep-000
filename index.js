b78d0e968be65b173ee732c31814f0e6632744d0
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-09 04:16:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if(cardNumber){
     (`Your total cost is $${total} which will be charged to the card ${cardNumber}.`);
-    for (var i = cart.length; i>=0;i--){
-      cart.pop();}
+    for (var i = cart.length; i>=0;i--) {
+      cart.pop();
     }
-}
+    } else ("Sorry, we don\'t have a credit card on file for you.")
 }
