09b93402dbb0c42a66f5ba108d4d9821cc578589
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-09 04:38:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function removeFromCart(item) {
 }
 function placeOrder(cardNumber) {
   if(cardNumber){
-    console.log(`Your total cost is $${total} which will be charged to the card ${cardNumber}.`);
+    (`Your total cost is $${total} which will be charged to the card ${cardNumber}.`);
     for (var i = cart.length; i>=0;i--) {
       cart.pop();
     }
