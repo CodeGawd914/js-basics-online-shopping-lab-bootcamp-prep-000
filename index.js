8117fd9cf0ea84e867be6cad612dc7ec0f223241
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-09 04:52:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,10 +53,10 @@ function removeFromCart(item) {
   return "That item is not in your cart."
 }
 function placeOrder(cardNumber) {
-  if(cardNumber){
+  if(cardNumber === undefined){("Sorry, we don\'t have a credit card on file for you.")
     `Your total cost is $${total()} which will be charged to the card ${cardNumber}.`;
     for (var i = cart.length; i>=0;i--) {
       cart.pop();
     }
-    } else ("Sorry, we don\'t have a credit card on file for you.")
+    } else 
 }
