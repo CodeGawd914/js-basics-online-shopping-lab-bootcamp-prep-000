40c2a0e3fdb196f84b7a51a14d748f4ea1714a90
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-09 03:46:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
      return cart
      }
   }
-  return "That item is not in your car"
+  return "That item is not in your cart."
 function placeOrder(cardNumber) {
   // write your code here
 }
